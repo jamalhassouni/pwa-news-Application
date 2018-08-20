0c3b1cdbb8e4c5ccf5e05ff02fad188cb337b01d
jamal hassouni <jamalhassouni13@gmail.com>
2018-08-20 19:47:48 +0100

    feat: Use fetch event to get data from the cache Check if the site is offline or not and call data from the cache as well as store data dynamically

--- sw.js
+++ sw.js
@@ -15,6 +15,37 @@ self.addEventListener('install', async event => {
 
 self.addEventListener('fetch', event => {
   
- console.log('fetch');
+ const req  = event.request;
+ const url =  new URL(req.url);
+
+ if(url.origin === location.origin){
+  
+   event.respondWith(cacheFirst(req));
+ 
+ }else {
+ 	event.respondWith(networkFirst(req));
+ }
 
 });
+
+async function cacheFirst(req) {
+
+  // check if have response request
+  const cachedResponse  = await caches.match(req);
+  return cachedResponse || fetch(req);
+  
+
+}
+
+async function networkFirst(req){
+
+ const cache =  await caches.open('news-dynamic');
+  try {
+     const res = await fetch(req);
+     cache.put(req, res.clone());
+     return res;
+  } catch(e) {
+   return 	await cache.match(req);
+  }
+ 
+}
